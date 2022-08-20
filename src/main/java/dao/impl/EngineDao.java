4f58d0e1ae75c417c574eea6d675be0c973c23a1
krasnovss91 <krasnovstanislav91@gmail.com>
2022-08-20 14:09:32 +0400

    Dao-1

---
+++ src/main/java/dao/impl/EngineDao.java
@@ -0,0 +1,28 @@
+package dao.impl;
+
+import dao.AbstractDao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class EngineDao implements AbstractDao {
+    @Override
+    public List<Object> getAllDao() {
+        return null;
+    }
+
+    @Override
+    public Object getByIdDao(Integer id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void addDao(Object object) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteByIdDao(Integer id) throws SQLException {
+
+    }
+}
