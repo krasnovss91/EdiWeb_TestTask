0474d9cfa038f39f18f0036185a688036213ba3a
krasnovss91 <krasnovstanislav91@gmail.com>
2022-08-22 15:45:47 +0400

    ServiceMethods-1

--- src/main/java/service/CarService.java
+++ src/main/java/service/CarService.java
@@ -1,4 +1,27 @@
 package service;
 
+import entity.Car;
+
+import java.util.List;
+
 public class CarService {
+    public Car getCarById(Integer id){
+
+    }
+
+    public List<Car> getAllCars(){
+
+    }
+
+    public void addCar(Car car){
+
+    }
+
+    public void updateCar(Car car){
+
+    }
+
+    public void deleteCarById(Car car){
+
+    }
 }
