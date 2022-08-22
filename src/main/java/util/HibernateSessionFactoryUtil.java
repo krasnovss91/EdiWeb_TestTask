9ed7c793467698930e9135f640679530c4725091
krasnovss91 <krasnovstanislav91@gmail.com>
2022-08-22 12:07:02 +0400

    SessionFactory

---
+++ src/main/java/util/HibernateSessionFactoryUtil.java
@@ -0,0 +1,34 @@
+package util;
+
+import entity.Car;
+import entity.Carcase;
+import entity.Engine;
+import entity.Transmission;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactoryUtil {
+    private static SessionFactory sessionFactory;
+
+    private HibernateSessionFactoryUtil() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(Car.class);
+                configuration.addAnnotatedClass(Carcase.class);
+                configuration.addAnnotatedClass(Engine.class);
+                configuration.addAnnotatedClass(Transmission.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+
+            } catch (Exception e) {
+                System.out.println("Исключение!" + e);
+            }
+        }
+        return sessionFactory;
+    }
+}
